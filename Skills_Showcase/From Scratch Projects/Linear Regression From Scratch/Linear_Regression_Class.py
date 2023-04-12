a1d9992a5754ebd9054729539e152c869ee42e5a
Shane Dabney <108029118+sdabney5@users.noreply.github.com>
2023-04-12 17:49:02 -0400

    Add files via upload

---
+++ Skills_Showcase/From Scratch Projects/Linear Regression From Scratch/Linear_Regression_Class.py
@@ -0,0 +1,54 @@
+class LinearRegression:
+    
+    #initialize hyperparameters
+    def __init__(self, alpha = 0.001, num_iterations = 1000):
+        self.alpha = alpha
+        self.num_iterations = num_iterations
+        self.theta = None
+        
+    def fit(self, X, y):
+        # Add a column of 1's to X to represent the intercept
+        X = X.insert(0, "ONES", 1) 
+        
+        # Rows (m) and columns (n)
+        m , n = X.shape
+        
+        # Initialize a list of 0's of length "n" for first theta values
+        self.theta = [0]*n         
+        
+        #Hypothesis function
+        for _ in range(self.num_iterations):
+            # Create a 0's list of predicted values for y_hat
+            y_hat = [0]*m 
+            for i in range(m):
+                 for j in range(n):
+                        y_hat[i] += (X[i][j] * self.theta[j])  #this is the hypothesis function.
+                        
+                
+            #Gradient calculation of MSE (J(theta)) w.r.t. theta[j]
+            dJ_dtheta = [0]*n 
+            
+            for j in range(n):
+                for i in range(m):
+                    dJ_dtheta[j] += 1/m * ((y_hat[i] - y[i]) * X[i][j]) # The partial derivative of the MSE w.r.t theta[j]
+                    
+             
+            #update theta values
+            for j in range(n):
+                self.theta[j] -=  self.alpha * dJ_dtheta[j]
+            
+    def predict(self, X):
+        #Add a column of 1's to X to represent the intercept
+        X = X.insert(0, "ONES", 1) 
+        
+        # Rows (m) and columns (n)
+        m , n = X.shape 
+        
+        #Create a list of zeros for predictions corresponding to each row
+        y_prediction = [0]*m 
+        
+        #Compute predictions
+        for i in range(m):
+            for j in range(n):
+                y_prediction[i] += X[i][j] * self.theta[j]
+        return y_prediction
